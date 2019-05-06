f87a0445592dabb04fd4157988eb7f68c15c5978
Don Nguyen <dnguyen@pivotal.io>
2019-05-06 10:49:05 -0400

    Support latest dagger

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -10,34 +11,40 @@ import (
 	"github.com/sclevine/spec/report"
 )
 
+var (
+	bpDir, nodeCompatURI, nodeURI, npmURI string
+)
+
 func TestIntegration(t *testing.T) {
+	var err error
+
+	Expect := NewWithT(t).Expect
+	bpDir, err = dagger.FindBPRoot()
+	Expect(err).NotTo(HaveOccurred())
+	nodeCompatURI, err = dagger.PackageBuildpack(bpDir)
+	Expect(err).ToNot(HaveOccurred())
+	defer os.RemoveAll(nodeCompatURI)
+
+	nodeURI, err = dagger.GetLatestBuildpack("nodejs-cnb")
+	Expect(err).ToNot(HaveOccurred())
+	defer os.RemoveAll(nodeURI)
+
+	npmURI, err = dagger.GetLatestBuildpack("npm-cnb")
+	Expect(err).ToNot(HaveOccurred())
+	defer os.RemoveAll(npmURI)
+
 	spec.Run(t, "Integration", testIntegration, spec.Report(report.Terminal{}))
 }
 
 func testIntegration(t *testing.T, when spec.G, it spec.S) {
-	var (
-		err    error
-		bp     string
-		nodeBP string
-		npmBP  string
-	)
-
+	var Expect func(interface{}, ...interface{}) GomegaAssertion
 	it.Before(func() {
-		RegisterTestingT(t)
-
-		bp, err = dagger.PackageBuildpack()
-		Expect(err).ToNot(HaveOccurred())
-
-		nodeBP, err = dagger.GetLatestBuildpack("nodejs-cnb")
-		Expect(err).ToNot(HaveOccurred())
-
-		npmBP, err = dagger.GetLatestBuildpack("npm-cnb")
-		Expect(err).ToNot(HaveOccurred())
+		Expect = NewWithT(t).Expect
 	})
 
 	when("when heroku-postbuild and heroku-prebuild scripts are in package.json", func() {
 		it("rewrites them as preinstall and postinstall scripts", func() {
-			app, err := dagger.PackBuild(filepath.Join("testdata", "pre_post_commands"), bp, nodeBP, npmBP)
+			app, err := dagger.PackBuild(filepath.Join("testdata", "pre_post_commands"), nodeCompatURI, nodeURI, npmURI)
 			Expect(err).ToNot(HaveOccurred())
 			defer app.Destroy()
 
